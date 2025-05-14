6899e82a954a4fce35f24da54879599648356388
Alan Barroncas <alanbarroncas@gmail.com>
2025-05-14 16:07:52 -0400

    Ref: Construindo o fluxo de eventos

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,15 +1,17 @@
 // Arquivo com a lógica de autenticação //
 
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-     
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    // Simulação simples de login //
-    if(username === 'admin' && password === '1234') {
-        window.location.href = 'pages/admin.html'; // aqui será uma pagina que vai ser redirecionada após o login
-    } else {
-        document.getElementById('errorMsg').textContent = 'Usuário ou senha inválidos';
-    }
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Simulação simples de login //
+  if (username === "admin" && password === "1234") {
+    // aqui será uma pagina que vai ser redirecionada após o login
+    window.location.href = "../pages/admin/dashboard.html";
+  } else {
+    document.getElementById("errorMsg").textContent =
+      "Usuário ou senha inválidos";
+  }
 });
